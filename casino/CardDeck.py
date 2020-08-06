d4ac05ca49f99ebbb3bab29ebf3727694ad1a06f
adam050986@yahoo.fr <W@c00l00>
2020-08-06 23:00:18 +0200

    add deck

---
+++ casino/CardDeck.py
@@ -0,0 +1,34 @@
+# from https://www.youtube.com/watch?v=kzJHgcp5MLo
+import random
+
+# list holders to place cards
+cardfaces = []
+suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
+royals = ["J", "Q", "K", "A"]
+deck = []
+
+# Start using loop to add content
+for i in range (2,11):
+    cardfaces.append(str(i))
+    # this adds 2-10 and convert them to string data
+
+for j in range (4):
+    cardfaces.append(royals[j])
+    #this will add the royal faces
+
+# need to attach the suits to it
+
+for k in range(4):
+    for l in range(13):
+        card = (cardfaces[l] + " of " + suits[k])
+        deck.append(card)
+
+for m in range(52):
+    print(deck[m])
+
+# Melange le paquet
+random.shuffle(deck)
+print("")
+
+for m in range(52):
+    print(deck[m])
